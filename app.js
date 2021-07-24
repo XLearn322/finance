8488536582bb1c8664b1d78f626975b0be6ca719
bold <burnaa81@gmail.com>
2021-07-24 22:58:37 +0800

    finance dotroos ustgal hiine

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var uiController = (function(){
         incomeLabel: ".budget__income--value",
         expenseLabel: ".budget__expenses--value",
         persentageLabel: ".budget__expenses--percentage",
+        containerDiv: ".container",
+
     }
         return {
         getInput: function(){
@@ -67,11 +69,11 @@ var uiController = (function(){
       if (type === "inc") {
         list = DOMstrings.incomeList;
         html =
-          '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
+          '<div class="item clearfix" id="inc-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
       } else {
         list = DOMstrings.expenseList;
         html =
-          '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
+          '<div class="item clearfix" id="exp-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
       // Ð¢ÑÑ€ HTML Ð´Ð¾Ñ‚Ñ€Ð¾Ð¾ Ð¾Ñ€Ð»Ð¾Ð³Ð¾ Ð·Ð°Ñ€Ð»Ð°Ð³Ñ‹Ð½ ÑƒÑ‚Ð³ÑƒÑƒÐ´Ñ‹Ð³ REPLACE Ð°ÑˆÐ¸Ð³Ð»Ð°Ð¶ Ó©Ó©Ñ€Ñ‡Ð¸Ð»Ð¶
       html = html.replace("%id%", item.id);
@@ -146,6 +148,19 @@ var financeController = (function(){
             }
         },
 
+        deleteItem: function(type, id){
+            var ids = data.items[type].map(function(el){
+                return el.id;
+            });
+
+            var index = ids.indexOf(id);
+
+            if(index !== -1){
+                data.items[type].splice(index, 1);
+            }
+        },
+
+
         addItem: function(type, desc, val){
             var item, id;
 
@@ -215,6 +230,25 @@ var appController = (function(uiController, financeController){
             ctrlAddItem();
             }
         });
+
+        document.querySelector(DOM.containerDiv).addEventListener('click',function(event){
+            var id = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+            if(id){
+                var arr = id.split("-");
+                var type = arr[0];
+                var itemId = parseInt(arr[1]);
+
+                // console.log(type + ' : ' + itemId);
+
+                // 1. sanhuugin modulias type , id ashiglaad ustgana .
+                financeController.deleteItem(type, itemId);
+                // 2. delgets deerees ene elementiig ustgana
+
+                // 3. uldegdel tootsoog shinechilj haruulna
+            }
+            
+        })
     }
 
     return {
