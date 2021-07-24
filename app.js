0e2f0811abfcab0d1a357f326e4292a216ea2003
bold <burnaa81@gmail.com>
2021-07-24 17:02:54 +0800

    focus laj hiine

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var uiController = (function(){
         inputType: ".add__type",
         inputDescription: ".add__description",
         inputValue: ".add__value",
-        addBtn: ".add__btn"
+        addBtn: ".add__btn",
+        incomeList: ".income__list",
+        expenseList: ".expenses__list"
     }
         return {
         getInput: function(){
@@ -20,15 +22,33 @@ var uiController = (function(){
         getDOMstrings: function(){
             return DOMstrings;
         },
+
+        clearFields: function(){
+            var fields = document.querySelectorAll(DOMstrings.inputDescription + "," + DOMstrings.inputValue);
+
+            // convert list to array
+            var fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(el, index, array){
+                el.value = "";
+            });
+
+            fieldsArr[0].focus();
+
+            // for(var i = 0; i < fieldsArr.length; i++){
+            //     fieldsArr[i].value = "";
+            // }
+        },
+
         addListItem: function(item, type){
             // 1. orlogo zarlagiin elementiig aguulsan html iig beltgene.
             var html, list;
       if (type === "inc") {
-        list = ".income__list";
+        list = DOMstrings.incomeList;
         html =
           '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
       } else {
-        list = ".expenses__list";
+        list = DOMstrings.expenseList;
         html =
           '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
@@ -108,6 +128,8 @@ var appController = (function(uiController, financeController){
         var item = financeController.addItem(input.type, input.description, input.value);
         // 3. olj avsan ogogdluudee web deeree tohiroh hesegt gargana.
         uiController.addListItem(item, input.type);
+        uiController.clearFields();
+
         // 4. tosviig tootsoolno.
 
         // 5. etssiin uldegdel, tootsoog delgtetsend gargana.
